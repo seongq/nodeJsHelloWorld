965d3dfcdfd75af658083a58ced28d2bb3626901
seongq <36037178+seongq@users.noreply.github.com>
2020-07-14 11:19:08 +0900

    Dynamic Website with EJS

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = express();
 
 const port = 3000;
 
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+
+app.get("/hello", (req, res) => {
+	res.render("hello", { name: req.query.nameQuery });
+	console.log("query");
+	console.log(req.query);
+});
+
+app.get("/hello/:nameParam", function (req, res) {
+	res.render("hello", { name: req.params.nameParam });
+	console.log("params");
+	console.log(req.params);
+});
 
 app.listen(port, () => console.log(`server on! http://localhost:${port}`));
